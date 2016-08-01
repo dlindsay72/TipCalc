15577fac790160074afd5e2d602805e4c6afc1ee
Dan Lindsay <dlindsay72@hotmail.com>
2016-08-01 12:29:04 -0400

    added all views, labels, imageViews, textFields, and sliders to Storyboard as well as constraints

---
+++ TipBeeyatch!!/TipBeeyatchVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TipBeeyatch!!
+//
+//  Created by Dan Lindsay on 2016-08-01.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class TipBeeyatchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+
+}
+
