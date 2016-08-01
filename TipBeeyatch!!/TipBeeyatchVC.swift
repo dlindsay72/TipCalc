dee42df4856a441ab8834c271a50e1e1189183ae
Dan Lindsay <dlindsay72@hotmail.com>
2016-08-01 12:55:10 -0400

    textField will now store number value to totalLabel

--- TipBeeyatch!!/TipBeeyatchVC.swift
+++ TipBeeyatch!!/TipBeeyatchVC.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class TipBeeyatchVC: UIViewController {
+    
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var splitSlider: UISlider!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var splitSliderLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var splitAmountLabel: UILabel!
+    
+    var tipPercentAmount = Double()
+    var tipTotal = Double()
+  //  var totalBillAmount = Double()
+    var numberOfPersons = Int()
+    var splitTotalAmount = Double()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +34,25 @@ class TipBeeyatchVC: UIViewController {
     }
 
     
+    @IBAction func tipSliderMoved(_ sender: AnyObject) {
+        
+    }
+    
+   
+    @IBAction func splitSliderMoved(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func billAmountEntered(_ sender: AnyObject) {
+        
+        
+        if let totalBillAmount = Double(billAmountTextField.text!) {
+            totalAmountLabel.text = "$\(totalBillAmount)"
+        } else {
+            print("no amount entered")
+        }
+        
+    }
 
 
 }
